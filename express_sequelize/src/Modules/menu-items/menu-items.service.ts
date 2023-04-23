7f4227faa1a26dcd1f0ca735ec73b975fef850c0
mfaizan1 <mfaizanulhaq1@gmail.com>
2023-04-24 02:58:17 +0500

    feat: complete getMenuItems

--- express_sequelize/src/Modules/menu-items/menu-items.service.ts
+++ express_sequelize/src/Modules/menu-items/menu-items.service.ts
@@ -1,3 +1,5 @@
+import MenuItem from './entities/menu-item.entity'
+
 export class MenuItemsService {
 
   /* TODO: complete getMenuItems so that it returns a nested menu structure
@@ -76,6 +78,6 @@ export class MenuItemsService {
   */
 
   async getMenuItems() {
-    throw new Error('TODO in task 3');
+    return MenuItem.findAll({include: { all: true }})
   }
 }
